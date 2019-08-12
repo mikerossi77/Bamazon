4b171fcfec9ebc0cd72f3051ffcdacefd0ea45b9
Michael Rossi <michaelrossi@Michaels-MacBook-Pro.local>
2019-08-11 20:17:59 -0700

    finished

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,9 +17,74 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
     readProducts();
+});
 
+
+function updateQuantity(itemId, quantity, howMany, price) {
+    newQuantity = quantity-howMany;
+    var query = connection.query(
+        "UPDATE products SET ? WHERE ?",
+        [
+            {
+                stock_quantity: [newQuantity]
+            },
+            {
+                item_id: [itemId]
+            }
+        ],
+        function (err, res) {
+            if (err) throw err;
+            console.log(res.affectedRows + " Product(s) Ordered!\n");
+            console.log("Your purchase total is:  " + quantity * price);
+        }
+    );
+
+    connection.end();
+}
+
+
+function getQuantity(id) {
+    var myQuantity = 0;
+    connection.query("SELECT item_id, stock_quantity FROM products", function (err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        res.forEach(function (product) {
+            if (product.item_id == id) {
+                console.log(product.item_id);
+                myQuantity = product.stock_quantity;
+                console.log("myQuantity is:  " + product.stock_quantity)
+            }
+        })
+        connection.end();
+        console.log("Now myQuantity is:  " + myQuantity);
+        return myQuantity;
+    });
+    console.log("Now myQuantity is:  " + myQuantity);
+
+}
+
+
+function readProducts() {
+    console.log("Selecting all products...\n");
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        //console.log(res);
+        res.forEach(function (product) {
+            console.log("Item Number: " + product.item_id + ", " + product.product_name + ", Price: $" + product.price + ",  Quantity: " + product.stock_quantity);
+            // item_id: 8,
+            // product_name: 'Chromebook',
+            // department_name: null,
+            // price: '200',
+            // stock_quantity: 2
+        })
+        console.log("============")
+        getOrder();
+    });
+}
+
+function getOrder() {
     inquirer.prompt([
 
         {
@@ -36,103 +101,26 @@ connection.connect(function (err) {
     ]).then(function (answer) {
         console.log("Product Number: " + answer.myProduct);
         console.log("Quantity: " + answer.howMany);
-        var available = getQuantity(answer.myProduct);
-        if(Number(answer.howMany) < available) {
-            console.log("Sorry pal, we ain't got that many");
-        }
-    });
-});
-
-
-function createProduct() {
-    console.log("Inserting a new product...\n");
-    var query = connection.query(
-        "INSERT INTO products SET ?",
-        {
-            flavor: "Rocky Road",
-            price: 3.0,
-            quantity: 50
-        },
-        function (err, res) {
+        var myQuantity = 0;
+        var id = answer.myProduct;
+        var price = 0;
+        connection.query("SELECT item_id, stock_quantity, price FROM products", function (err, res) {
             if (err) throw err;
-            console.log(res.affectedRows + " product inserted!\n");
-            // Call updateProduct AFTER the INSERT completes
-            updateProduct();
-        }
-    );
+            res.forEach(function (product) {
+                if (product.item_id == id) {
+                    myQuantity = product.stock_quantity;
+                    price = product.price;
+                }
+            })
 
-    // logs the actual query being run
-    console.log(query.sql);
-}
-
-function updateProduct() {
-    console.log("Updating all Rocky Road quantities...\n");
-    var query = connection.query(
-        "UPDATE products SET ? WHERE ?",
-        [
-            {
-                quantity: 100
-            },
-            {
-                flavor: "Rocky Road"
+            var available = myQuantity;
+            if (Number(answer.howMany) > available) {
+                console.log("Sorry pal, we ain't got that many");
             }
-        ],
-        function (err, res) {
-            if (err) throw err;
-            console.log(res.affectedRows + " products updated!\n");
-            // Call deleteProduct AFTER the UPDATE completes
-            deleteProduct();
-        }
-    );
-
-    // logs the actual query being run
-    console.log(query.sql);
-}
-
-function deleteProduct() {
-    console.log("Deleting all strawberry icecream...\n");
-    connection.query(
-        "DELETE FROM products WHERE ?",
-        {
-            flavor: "strawberry"
-        },
-        function (err, res) {
-            if (err) throw err;
-            console.log(res.affectedRows + " products deleted!\n");
-            // Call readProducts AFTER the DELETE completes
-            readProducts();
-        }
-    );
-}
-function getQuantity(id) {
-    connection.query("SELECT stock_quantity FROM products", function (err, res) {
-        if (err) throw err;
-        // Log all results of the SELECT statement
-        //console.log(res);
-        res.forEach(function (product) {
-            if (product.item_id === id) {
-                return product.stock_quantity;
+            else {
+                updateQuantity(answer.myProduct, Number(myQuantity), answer.howMany, price);
             }
-        })
-        connection.end();
-    });
-}
-
-
-function readProducts() {
-    console.log("Selecting all products...\n");
-    connection.query("SELECT * FROM products", function (err, res) {
-        if (err) throw err;
-        // Log all results of the SELECT statement
-        //console.log(res);
-        res.forEach(function (product) {
-            console.log(product.product_name + ", Price: $" + product.price + ",  Quantity: " + product.stock_quantity);
-            // item_id: 8,
-            // product_name: 'Chromebook',
-            // department_name: null,
-            // price: '200',
-            // stock_quantity: 2
-        })
-        console.log("============")
+            
+        });
     });
 }
